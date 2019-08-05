e307462299fa85184931d865243ef8aea5ead3f9
Pablo Ortiz <pablo@ryaltoapp.com>
2019-08-05 16:24:16 +0100

    Initial Commit

---
+++ Sync.Money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sync.Money
+//
+//  Created by Pablo Rodriguez on 05/08/2019.
+//  Copyright © 2019 Pablo Ortiz Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
